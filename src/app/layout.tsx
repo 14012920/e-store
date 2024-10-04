f442303afe4b593c10f3c88f20e5556d453dda5f
manoj soni <sonimanoj038@gmail.com>
2024-10-04 10:44:46 +0530

    fb setup 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src={`https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=PageView&noscript=1`}
+            src={`https://www.facebook.com/tr?id=1063120718588441&ev=PageView&noscript=1`}
           />
         </noscript>
       </body>
