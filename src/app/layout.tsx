2f2f6419df49b13f79258b7934ec12b1e2937c95
manoj soni <sonimanoj038@gmail.com>
2024-10-04 10:43:48 +0530

    fb setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const customFont = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "E-STORE",
+  title: "DUOPRIMP STORE",
   description: "ONLINE E-COMMERCE-STORE",
 };
 
@@ -22,7 +22,32 @@ export default function RootLayout({
     <html lang="en">
       <Script src="https://checkout.razorpay.com/v1/magic-checkout.js" />
       <Script src="https://checkout.razorpay.com/v1/checkout.js" />
-      <body className={customFont.className}>{children}</body>
+      <body className={customFont.className}>
+        {children}
+
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1063120718588441');
+          fbq('track', 'PageView');
+        `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=PageView&noscript=1`}
+          />
+        </noscript>
+      </body>
     </html>
   );
 }
